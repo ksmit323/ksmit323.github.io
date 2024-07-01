95c72c9fa6d81ec2ff6a0f69f45b0c9aa4f8df60
Kenneth Smith <ksmit323@gmail.com>
2024-07-01 18:32:05 +0700

    refactor background

---
+++ src/components/CosmicBackground.tsx
@@ -0,0 +1,14 @@
+import StarField from './StarField';
+import Nebula from './Nebula';
+
+
+const CosmicBackground = () => {
+  return (
+    <>
+      <StarField />
+      <Nebula />
+    </>
+  );
+};
+
+export default CosmicBackground;
---
+++ src/components/StarField.tsx
@@ -0,0 +1,32 @@
+import { motion } from 'framer-motion';
+
+
+const StarField: React.FC = () => {
+  return (
+    <div className="fixed inset-0 pointer-events-none">
+      {[...Array(200)].map((_, i) => (
+        <motion.div
+          key={i}
+          className="absolute bg-white rounded-full"
+          style={{
+            width: Math.random() * 2 + 1 + 'px',
+            height: Math.random() * 2 + 1 + 'px',
+            top: Math.random() * 100 + '%',
+            left: Math.random() * 100 + '%',
+          }}
+          animate={{
+            opacity: [0, 1, 0],
+            scale: [0, 1, 0],
+          }}
+          transition={{
+            duration: Math.random() * 3 + 2,
+            repeat: Infinity,
+            repeatType: 'loop',
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default StarField;
