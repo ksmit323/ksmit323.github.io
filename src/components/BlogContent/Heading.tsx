fb47b2f5174d42d3427ff3021aade936fe1fd7ca
kss <ksmit323@gmail.com>
2024-07-04 17:00:53 +0700

    fix formatting

--- src/components/BlogContent/Heading.tsx
+++ src/components/BlogContent/Heading.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 const Heading: React.FC<{ children: ReactNode }> = ({ children }) => (
   <motion.h1
-    className="text-5xl font-bold mb-6 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-600"
+    className="text-5xl font-bold mb-6 justify-text text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-600 text-left"
     initial={{ opacity: 0, y: -50 }}
     animate={{ opacity: 1, y: 0 }}
     transition={{ duration: 0.8, delay: 0.2 }}
